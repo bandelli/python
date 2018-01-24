48fbf124e18ca80861b1825729281af0ace24e5a
Mari Martin <bandelli@gmail.com>
2018-01-23 22:36:20 -0200

    Python basico

---
+++ amopython.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+frase = 'Eu amo Python'
+print frase
+if len(frase) > 10:
+    if(frase == 'Eu amo Python'):
+        print 'Eu também'
+    else:
+        print 'Ham?'
+else:
+    print 'Tamanho insuficiente'
